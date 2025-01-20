3207df419e2cb2d1ccf6160660c293ba420814e5
Surendra.Reddy2 <surendra.reddy@pressganey.com>
2025-01-20 17:34:54 +0530

    test commit

---
+++ src/Utils/ValidateAPI.js
@@ -0,0 +1,26 @@
+const validator = require("validator");
+
+const validateAPI = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+
+  // Validate firstName and lastName
+  if (!firstName || firstName.length < 4 || firstName.length > 50) {
+    throw new Error("First name must be between 4 and 50 characters.");
+  }
+
+  if (!lastName || lastName.length < 4 || lastName.length > 50) {
+    throw new Error("Last name must be between 4 and 50 characters.");
+  }
+
+  // Validate emailId
+  if (!emailId || !validator.isEmail(emailId)) {
+    throw new Error("Invalid email address.");
+  }
+
+  // Validate password
+  if (!password || !validator.isStrongPassword(password)) {
+    throw new Error("Password must be strong (at least 8 characters, including letters, numbers, and special characters).");
+  }
+};
+
+module.exports = { validateAPI };
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongooseConnection = require("../src/config/mongooseConnection");
 const { User } = require("./models/UserModel");
+const { validateAPI } = require("./Utils/ValidateAPI");
 
 const app = express();
 app.use(express.json());
@@ -51,6 +52,35 @@ app.patch("/user", async (req, res) => {
     console.log(err.message);
   }
 });
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Prepare data for user
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      emailId: emailId,
+      password: password
+    };
+
+    // Validate the input data
+    validateAPI(req);
+
+    // Create a new User instance
+    const user = new User(data);
+
+    // Save the user to the database
+    const savedUser = await user.save();
+
+    // Respond with success message
+    res.status(201).send("User successfully signed up");
+  } catch (err) {
+    // Log error for debugging and respond with error message
+    console.error(err);
+    res.status(400).send({ error: err.message });
+  }})
 
 mongooseConnection().then(() =>
   app.listen(7777, () => {
