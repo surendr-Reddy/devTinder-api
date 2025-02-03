d8a5d15a64672852c4d35f366fa90880ff446eaa
Surendra.Reddy2 <surendra.reddy@pressganey.com>
2025-02-03 16:18:16 +0530

    dummny connection request added

--- src/models/conntectionRequest.js
+++ src/models/conntectionRequest.js
@@ -1,26 +1,41 @@
 const { mongoose } = require("mongoose");
 
-const connectionRequest = mongoose.Schema(
+const connectionRequestSchema = mongoose.Schema(
   {
-    formId: { type: String, required: true },
-    toId: { type: String, required: true },
+    fromUserId: { type:  mongoose.Schema.Types.ObjectId, required: true },
+    toUserId: { type:  mongoose.Schema.Types.ObjectId, required: true },
     status: {
       type: String,
       required: true,
-      enum: ["interested", "ignore"],
-    //   validate: {
-    //     validetor: (vaule) => {
-    //       const allwedfiled = ["interested", "ignore"];
-    //       const isvalid = allwedfiled.each((filed) => {
-    //         vaule.incudels(filed);
-    //       });
-    //       return isvalids;
-    //     },
-    //     message: "invalid status",
-    //   },
+      // enum: {
+      //   values: ["ignored", "interested", "accepted", "rejected"],
+      //   message: `{VALUE} is incorrect status type`,
+      // },
+      // enum: ["interested", "ignore"],
+      validate: {
+        validator: (vaule) => {
+          const allwedfiled = ["interested", "ignore"];
+          const isvalid = allwedfiled.includes(vaule) //the vaule single string so we can use this but if it is array then use bellow
+          //   const isvalid = allwedfiled.some((filed) => {
+          //     vaule.includes(filed); // here the vaule is string so we cant use the .includes
+          //   });
+             return isvalid;
+        },
+        message: "invalid status",
+      },
     },
   },
   { timeStamp: true }
 );
-const connectionRequestModel=mongoose.model("connectionRequest",connectionRequest)
-module.exports={connectionRequestModel}
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  console.log(connectionRequest);
+  
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(this.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
+const connectionRequestModel = mongoose.model("connectionRequestSchema", connectionRequestSchema)
+module.exports = { connectionRequestModel }
--- src/routers/request.js
+++ src/routers/request.js
@@ -1,19 +1,51 @@
 const express = require("express");
 const requestRouter = express.Router();
+const mongoose =require("mongoose")
 const { connectionRequestModel } = require("../models/conntectionRequest");
+const { User } = require("../models/UserModel")
 requestRouter.get("/send/:status/:userId", async (req, res) => {
-  const user = req.user;
-  console.log(user);
-  
-  const touserId = req.params.userId;
-  const status1 = req.params.status;
-console.log(touserId,status1);
+  try {
+    const user = req.user;
+    const userID = user._id
 
-  const data = { formId: user._id, toId: touserId, status: status1 };
-  const connectionRequest = new connectionRequestModel(
-    data,
-  );
-  await connectionRequest.save();
-  res.status(201).json({status:"success",message:"request sent successfuly",data:connectionRequest})
+
+    const touserId = req.params.userId;
+    const status = req.params.status;
+    if (!status) {
+      return res.status(400).json({ status: "Error", message: "status Not Found" })
+    }
+    const allowedFields = ["interested", 'ignore'];
+    const isStatusAllowed = allowedFields.includes(status)
+
+
+
+    if (!isStatusAllowed) {
+      return res.status(400).json({ status: "Error", message: `status ${status} not Allowed` })
+    }
+    const isValidToUserId = await User.findById({ _id: touserId })
+    if (!touserId || !isValidToUserId||mongoose.Types.ObjectId.isValid(touserId)) {
+      return res.status(404).json({ status: "Error", message: "User Not Found!" })
+    }
+    // find() gives the return [] or array of result so used findone
+    const existingConnectionRequest = await connectionRequestModel.findOne({
+      $or: [{ fromUserId:userID, toUserId:touserId }, { fromUserId: touserId, toUserId: userID }]
+    })
+    console.log(existingConnectionRequest);
+
+
+    if (existingConnectionRequest) {
+      return res.status(400).json({ status: "Error", message: "Connection Request Already Exists!!" })
+    }
+
+    const data = { fromUserId: userID, toUserId: touserId, status: status };
+    const connectionRequest = new connectionRequestModel(
+      data,
+    );
+    await connectionRequest.save();
+    res.status(201).json({ status: "success", message: "request sent successfuly", data: connectionRequest })
+  } catch (err) {
+    res.status(500).json({ status: "Error", message: "fail to send request", data: { Type: err.message } })
+  }
+
 });
-module.exports={ requestRouter };
+module.exports = { requestRouter };
